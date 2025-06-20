685d741596658d09a81a3986b29b6b39d7e62092
Dhiraj Kumar Bhakta <bhaktadhiraj@gmail.com>
2025-06-20 22:07:28 +0530

    added folder structure

--- Frontend/src/pages/ProjectPlayground.jsx
+++ Frontend/src/pages/ProjectPlayground.jsx
@@ -1,18 +1,41 @@
 import { useParams } from "react-router-dom"
 import { EditorComponent } from "../components/molecules/EditorComponent/EditorComponent";
-import { EditorButton } from "../components/atoms/EditorButton/EditorButton";
-
+import {EditorButton} from '../components/atoms/EditorButton/EditorButton'
+import { TreeStructure } from "../components/organism/TreeStructure/TreeStructure";
+import { useEffect } from "react";
+import { useTreeStructureStore } from "../store/treeStructureStore.js";
 export const ProjectPlayground = () => {
 
-    const { projectId } = useParams();
+    const {projectId: projectIdFromUrl } = useParams();
 
-   return(
-    <>
-        ProjectId : {projectId}
-        <EditorButton isActive={true}/>
-        <EditorButton isActive={false}/>
+    const { setProjectId, projectId } = useTreeStructureStore();
 
-        <EditorComponent/>
-    </>
-   )
+    useEffect(() => {
+        setProjectId(projectIdFromUrl);
+    }, [setProjectId, projectIdFromUrl]);
+
+    return (
+        <>
+            Project Id: {projectIdFromUrl}
+            { projectId && (
+                <div
+                    style={{
+                        backgroundColor: "#333254",
+                        paddingRight: "10px",
+                        paddingTop: "0.3vh",
+                        minWidth: "250px",
+                        maxWidth: "25%",
+                        height: "99.7vh",
+                        overflow: "auto"
+                    }}
+                >
+                    <TreeStructure />
+                </div>
+            )}
+            <EditorComponent />
+            <EditorButton isActive={false} /> 
+            <EditorButton isActive={true}/> 
+            
+        </>
+    )
 }
